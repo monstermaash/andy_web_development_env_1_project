1d4e50e2a651676626aed660a5f0232897f8eb5f
andy vo <andy.vo170@hotmail.com>
2023-06-05 17:27:16 -0400

    Created index.php and about.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="CSS/main.css">
+</head>
+<body>
+  <h1>web environment introduction</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione dolores nulla, in accusantium adipisci culpa. Distinctio hic maiores necessitatibus voluptatum! Architecto reiciendis earum amet in repudiandae temporibus reprehenderit quis ex!</p>
+  <hr>
+  <h2 class="white-on-black">About the Course</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro laudantium quam eaque iste atque similique doloribus suscipit minima ut doloremque. Ad ex modi voluptatem delectus repellendus blanditiis beatae necessitatibus aut. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam maiores, minima vel, dignissimos, recusandae animi ex consectetur nihil assumenda quo incidunt reprehenderit modi omnis sit asperiores dolores officia nesciunt ut!</p>
+  <img src="Assets/images/vanier.jpg" alt="image of vanier college" width="50%">
+  <br>
+  <a href="https://vaniercollege-lea.omnivox.ca/" target="_blank">Link to Omnivox Lea</a>
+  <script src="JS/main.js"></script>
+</body>
+</html>
